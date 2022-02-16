349b4dea5a3bcb2c1d9ead03973b9e406386b08b
dhruvarya45 <75114216+dhruvarya45@users.noreply.github.com>
2022-02-16 11:34:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,152 +1,102 @@
-import cv2 as cv
-import numpy as np
+import cv2
+import keras.models
+
+print('Setting UP')
 import os
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils.np_utils import to_categorical
+import pytesseract
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+# from tensorflow.keras.models import load_model
+import numpy as np
+import imutils
+# from imutils.perspective import four_point_transform
+from utlis import *
+import SudokuSolver
+import cv2 as cv
+pytesseract.pytesseract.tesseract_cmd='Tesseract-OCR\\tesseract.exe'
 
-from keras.models import Sequential
-from keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dropout,Flatten
-from keras.layers.convolutional import Conv2D,MaxPooling2D
-import pickle
+# model=initializePredectionModel()
+model = keras.models.load_model('ocr.h5')
+# 1. Preparing the image
+img=cv.imread('3.jpeg')
+img=cv.resize(img,(450,450),interpolation=cv.INTER_CUBIC)
+cv.imshow("Image", img)
 
-#########################################
-path='myData'
-testRatio=0.2
-validationRatio=0.2
-imageDimensions=(32,32,3)
+# print(pytesseract.image_to_boxes(img))
 
-batchSizeVal=50
-epochsVal=10
-#########################################
+imgBlank=np.zeros((450,450,3),np.uint8)
+gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+imgBlur=cv.GaussianBlur(gray,(5,5),1)
+# cv.imshow("Blurred Image", imgBlur)
+imgThreshold=cv.adaptiveThreshold(imgBlur,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY_INV,11,4)
+cv.imshow("Threshold Image", imgThreshold)
+print(img.shape)
+print(imgThreshold.shape)
 
-images=[]
-classNo=[]
-myList=os.listdir(path)
-print("Total No of Classes Detected",len(myList))
-noOfClasses=len(myList)
-print("Importing classes ........")
-for x in range(0,noOfClasses):
-    myPicList=os.listdir(path+"/"+str(x))
-    for y in myPicList:
-        curImg=cv.imread(path+"/"+str(x)+"/"+y)
-        curImg=cv.resize(curImg,(imageDimensions[0],imageDimensions[1]))
-        images.append(curImg)
-        classNo.append(x)
-    print(x, end=" ")
-print(" ")
+# 2. Finding all contours
 
-print("Total Images in Image List =", len(images))
-print("Total IDs in classNo List =", len(classNo))
-images=np.array(images)
-classNo=np.array(classNo)
-print(images.shape)
+imgContours=imgThreshold.copy()
+imgBigContour=imgThreshold.copy()
+contours, hierarchies = cv.findContours(imgThreshold, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+cv.drawContours(imgContours, contours, -1, (0,255,255),1)
 
-# Splitting the data
+# 3.Find the biggest contour
+biggest,maxArea=biggestContour(contours)
+print(biggest)
+if biggest.size!=0:
+    biggest=reorder(biggest)
+    cv.drawContours(imgBigContour,biggest,-1,(0,0),25)
+    pts1=np.float32(biggest)
+    pts2=np.float32([[0,0],[450,0],[0,450],[450,450]])
+    matrix=cv.getPerspectiveTransform(pts1,pts2)
+    imgWrapColored=cv.warpPerspective(img,matrix,(450,450))
+    imgDetectedDigits=img.copy()
+    # imgWrapColored=cv.cvtColor(imgThreshold,cv.COLOR_BGR2GRAY)
+    cv.imshow("Biggest Contour", imgBigContour)
+    cv.imshow("Image", imgWrapColored)
 
-x_train,x_test,y_train,y_test=train_test_split(images,classNo,test_size=testRatio)
-x_train,x_validation,y_train,y_validation=train_test_split(x_train,y_train,test_size=validationRatio)
-print(x_train.shape)
-print(x_test.shape)
-print(x_validation.shape)
+   #4. Split the Image and find each digit available
+    imgSolvedDigits=imgWrapColored.copy()
+    boxes=split_boxes(imgWrapColored)
+    print(len(boxes))
+    cv.imshow("Sample", boxes[71])
+    numbers=getPredection(boxes,model)
+    print(numbers)
+    imgDetectedDigits=displayNumbers(imgDetectedDigits,numbers,color=(255,0,255))
+    # cv.imshow("Image Detected Digits", imgDetectedDigits)
+    numbers=np.asarray(numbers)
+    posArray=np.where(numbers > 0,0,1)
+    print(posArray)
 
-noOfSamples=[]
-for x in range(0,noOfClasses):
-    # print(len(np.where(y_train ==x)[0]))
-    noOfSamples.append(len(np.where(y_train ==x)[0]))
-print(noOfSamples)
+    ####5.Find the solution of the sudoku board
+    board=np.array_split(numbers,9)
+    try:
+        sudokuSolver.solve(board)
+    except:
+        pass
+    print(board)
+    flatList=[]
+    for sublist in board:
+        for item in sublist:
+            flatList.append(item)
+    solvedNumbers=flatList*posArray
+    imgSolvedDigits=displayNumbers(imgSolvedDigits,solvedNumbers)
 
-plt.figure(figsize=(10,5))
-plt.bar(range(0,noOfClasses),noOfSamples)
-plt.title("No of Images for each class")
-plt.xlabel("Class ID")
-plt.ylabel("Number of Images")
-plt.show()
+    ####6. Overlay Solution
+    pts2=np.float32(biggest)
+    pts1=np.float32([[0,0],[450,0],[0,450],[450,450]])
+    matrix=cv2.getPerspectiveTransform(pts1,pts2)
+    imgInvWarpColored=img.copy()
+    imgInvWarpColored=cv.warpPerspective(imgSolvedDigits,matrix,(450,450))
+    inv_perspective=cv.addWeighted(imgInvWarpColored,1,img,0.5,1)
+    imgDetectedDigits=drawGrid(imgDetectedDigits)
+    imgSolvedDigits=drawGrid(imgSolvedDigits)
 
+    # imageArray=([img,imgThreshold,imgContours,imgBigContour],[imgDetectedDigits,imgSolvedDigits,imgInvWarpColored,inv_perspective])
+    # stackedImage=stackImages(imageArray,1)
+    # cv.imshow('Stacked Images', inv_perspective)
 
-def preProcessing(img):
-    img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    img=cv.equalizeHist(img)
-    img=img/255
-    return img
+else:
+   print("No Sudoku Found")
 
-# img=preProcessing(x_train[30])
-# img=cv.resize(img,(300,300))
-# cv.imshow("PreProcessed Image", img)
-# cv.waitKey(0)
-
-x_train=np.array(list(map(preProcessing,x_train)))
-x_test=np.array(list(map(preProcessing,x_test)))
-x_validation=np.array(list(map(preProcessing,x_validation)))
-
-
-x_train=x_train.reshape(x_train.shape[0],x_train.shape[1],x_train.shape[2],1)
-x_test=x_test.reshape(x_test.shape[0],x_test.shape[1],x_test.shape[2],1)
-x_validation=x_validation.reshape(x_validation.shape[0],x_validation.shape[1],x_validation.shape[2],1)
-
-dataGen=ImageDataGenerator(width_shift_range=0.1,height_shift_range=0.1,zoom_range=0.2,shear_range=0.1,rotation_range=10)
-dataGen.fit(x_train)
-
-y_train= to_categorical(y_train,noOfClasses)
-y_test=to_categorical(y_test,noOfClasses)
-y_validation=to_categorical(y_validation,noOfClasses)
-
-def myModel():
-    noOfFilters=60
-    sizeOfFilter1=(5,5)
-    sizeOfFilter2=(3,3)
-    sizeOfPool=(2,2)
-    noOfNode=500
-
-    model= Sequential()
-    model.add((Conv2D(noOfFilters,sizeOfFilter1,input_shape=(imageDimensions[0],imageDimensions[1],1),activation='relu')))
-    model.add((Conv2D(noOfFilters,sizeOfFilter1,activation='relu')))
-    model.add(MaxPooling2D(pool_size=sizeOfPool))
-    model.add((Conv2D(noOfFilters//2, sizeOfFilter2, activation='relu')))
-    model.add((Conv2D(noOfFilters//2, sizeOfFilter2, activation='relu')))
-    model.add(MaxPooling2D(pool_size=sizeOfPool))
-    model.add(Dropout(0.5))
-
-    model.add(Flatten())
-    model.add(Dense(noOfNode,activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(noOfClasses, activation='softmax'))
-    model.compile(Adam(learning_rate=0.001), loss='categorical_crossentropy',metrics=['accuracy'])
-    return model
-
-model=myModel()
-print(model.summary())
-
-stepsPerEpochVal=len(x_train)//batchSizeVal
-history = model.fit_generator(dataGen.flow(x_train,y_train,batch_size=batchSizeVal),steps_per_epoch=stepsPerEpochVal,epochs=epochsVal,validation_data=(x_validation,y_validation),shuffle=1)
-
-plt.figure(1)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.legend(['training','validation'])
-plt.title('Loss')
-plt.xlabel('epoch')
-plt.show()
-plt.figure()
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.legend(['training','validation'])
-plt.title('Accuracy')
-plt.xlabel('epoch')
-plt.show()
-score=model.evaluate(x_test,y_test,verbose=0)
-print('Test Score =', score[0])
-print('Test Accuracy =', score[1])
-
-import tensorflow as tf
-model.save('ocr.h5')
-pickle_out=open("Resources/model_trained.p","wb")
-pickle.dump(model,pickle_out)
-pickle_out.close()
-
+cv.waitKey(0)
 
